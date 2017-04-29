66bbff6309715331bae5bb8ee41b80acab22b0af
Jared Greene <jmg2227@columbia.edu>
2017-04-28 23:37:44 -0400

    fucking bug found

--- scripts/vocal_interface.py
+++ scripts/vocal_interface.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 import requests
-#import rospy
+import rospy
 import time
-#from std_msgs.msg import String
-#from spoken_interaction.msg import VerbalRequest, VerbalResponse, KeyValue
+from std_msgs.msg import String
+from spoken_interaction.msg import VerbalRequest, VerbalResponse, KeyValue
 from datetime import datetime
 
 from random import choice
@@ -12,17 +12,18 @@ from string import ascii_lowercase
 
 generateSessionId = lambda x: ''.join(choice(ascii_lowercase) for i in range(x))
 
-#
-# def build_mock_response():
-#     verbalInput = VerbalRequest()
-#     verbalInput.timestamp  = str(datetime.now())
-#     verbalInput.phrase      = "Please remember that this is the kitchen"
-#     verbalInput.action_id   = "navigate_to_landmark"
-#     kv1                     = KeyValue()
-#     kv1.key = 'landmark'
-#     kv1.value = 'table'
-#     verbalInput.params      = [kv1]
-#     return verbalInput
+
+def build_mock_response():
+    verbalInput = VerbalRequest()
+    verbalInput.timestamp  = str(datetime.now())
+    verbalInput.phrase      = "Please remember that this is the kitchen"
+    verbalInput.action_id   = "navigate_to_landmark"
+    kv1                     = KeyValue()
+    kv1.key = 'landmark'
+    kv1.value = 'table'
+    verbalInput.params      = [kv1]
+    return verbalInput
+
 if __name__ == "__main__":
     root = "https://api.ai/v1/"
     query = root + "query"
@@ -36,17 +37,17 @@ if __name__ == "__main__":
             "verify"      :'false'
     }
 
-    # # Create a node
-    # rospy.init_node("vocal_request_handler")
-    # command_pub = rospy.Publisher("verbal_input", VerbalRequest, queue_size = 20)
-    # print "fire"
-    # while True:
-    #     command_pub.publish(build_mock_response())
-    #     print "published"
-    #     time.sleep(10)
+    # Create a node
+    rospy.init_node("vocal_request_handler")
+    command_pub = rospy.Publisher("verbal_input", VerbalRequest, queue_size = 20)
+    print "fire"
+    while True:
+        command_pub.publish(build_mock_response())
+        print "published"
+        time.sleep(10)
 
-    queryInput = raw_input("give me your voice!")
-    payload["query"] = queryInput
-    print payload
-    queryReq = requests.get(query, params=payload)
-    print queryReq.text
+    # queryInput = raw_input("give me your voice!")
+    # payload["query"] = queryInput
+    # print payload
+    # queryReq = requests.get(query, params=payload)
+    # print queryReq.text
